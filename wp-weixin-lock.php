38dabe2f0ace2d72f7d6831b8da904c1caceab6e
midoks <midoks@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-04-18 17:09:06 +0000

    5.2.6
    git-svn-id: https://plugins.svn.wordpress.org/wp-weixin-robot/tags/5.2@897561 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ wp-weixin-lock.php
@@ -0,0 +1,33 @@
+<?php
+//锁机制实现类
+class wp_weixin_lock{
+
+	public $conf = array(	
+		'link' => 'mysql', //memcache, mysql(默认),
+
+	);
+	private $linkID = null;
+
+	public function __construct(){	
+		switch($this->conf['link']){
+			case 'memcache':
+				include(WEIXIN_ROOT.'lock/wp_weixin_memcache.php');
+				$this->linkID = new wp_weixin_memcache_lock();break;
+			case 'memcache_bae':
+				include(WEIXIN_ROOT.'lock/wp_weixin_memcache_bae.php');
+				$this->linkID = new wp_weixin_memcache_bae_lock();break;
+			case 'mysql':
+			default:
+				include(WEIXIN_ROOT.'lock/wp_weixin_mysql.php');
+				$this->linkID = new wp_weixin_mysql_lock();
+				break;
+		}
+	}
+
+	public function __call($method, $args){
+		return call_user_func_array(array($this->linkID, $method), $args);
+	}
+
+
+}
+?>
